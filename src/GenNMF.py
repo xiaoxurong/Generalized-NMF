e1374b08f206e1564b464fde326fd23afdcf13af
123xrong <xrong8@wisc.edu>
2025-10-28 12:15:01 -0500

    Update GenNMF.py

--- src/GenNMF.py
+++ src/GenNMF.py
@@ -306,7 +306,7 @@ def ssc_omp_nmf_baseline(X, r, K, true_labels, max_iter=1000, random_state=None,
     # Step 1: SSC clustering
     # use the ssc_omp function to do the clustering
     model = SparseSubspaceClusteringOMP(n_clusters=K, affinity='nearest_neighbors', random_state=random_state)
-    print("Fitting SSC-OMP model... on data with shape:", X.shape)
+    print("Fitting SSC-OMP model... on data with shape:", X.T.shape)
     model.fit(X.T)
     pred_labels = model.labels_
 
